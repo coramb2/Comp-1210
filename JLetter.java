1378509c3c56fefd4413b57dd38bd5d0e559f5a5
Cora <104588809+coramb2@users.noreply.github.com>
2025-08-22 18:51:47 -0500

    Module 1

---
+++ JLetter.java
@@ -0,0 +1,27 @@
+/**
+*Program to print the letter J as a large block letter 
+*composed of the characters 'J', 'A', 'V', 'A'.
+*
+*Project 1
+*@author Cora Baldwin
+*@version January 20, 2022
+*/
+public class JLetter {
+/**
+*Uses 10 print statements to create the pattern of the
+*J and has a width of 12 characters.
+*@param args Command line arguments
+*/
+   public static void main(String[] args) {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}
---
+++ MyLifeGoals.java
@@ -0,0 +1,27 @@
+/**
+*Program to print my short, medium, and long term goals.
+*
+*Project 1
+*@author Cora Baldwin
+*@version January 19, 2022
+*/
+public class MyLifeGoals {
+/**
+*Prints my name then each of my different life goals.
+*@param args Command line arguments
+*/
+   public static void main(String[] args) {
+      System.out.println("Cora Baldwin");
+      System.out.println();
+      System.out.println("My short-term life goals are to keep earning "
+         + "A's in all my courses, and I would like to continue going "
+         + "to the gym consistently.");
+      System.out.println("My medium-term life goals are to be as involved "
+         + "in college life as I can. I also want to graduate in the spring "
+         + "of 2025 with a degree and a minor.");
+      System.out.println("My long-term life goals are still mostly undecided. "
+         + "I know I want to work on engineering projects, and I would like "
+         + "to invest in property.");
+    
+   }
+}
